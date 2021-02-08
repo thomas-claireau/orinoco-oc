83c5c80064c7938b5ff1df7bdebe4061ef949342
Thomas Claireau <thomas.claireau@gmail.com>
2021-02-08 18:49:56 +0100

    remove src

--- src/app.js
+++
@@ -1,3 +0,0 @@
-import OrinocoHeader from "./components/orinoco-header.js"
-
-customElements.define("orinoco-header", OrinocoHeader)
--- src/components/orinoco-header.js
+++
@@ -1,9 +0,0 @@
-export default class OrinocoHeader extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = `<header class="orinoco-header">
-            <h1>Orinoco</h1>
-            <div class="panier">Panier</div>
-        </header>`
-    }
-}
