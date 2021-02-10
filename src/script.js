31125bf5e563565c2fb57063273a2b025707ff6b
Thomas Claireau <thomas.claireau@gmail.com>
2021-02-10 13:45:22 +0100

    header scroll js

--- src/script.js
+++ src/script.js
@@ -6,4 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
     customElements.define("date-footer", DateFooter)
     customElements.define("products-list", ProductList)
     customElements.define("svg-file", SvgFile)
+
+    const header = document.querySelector('header')
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+            header.classList.add("scrolled")
+        } else {
+            header.classList.remove('scrolled')
+        }
+    })
 })
