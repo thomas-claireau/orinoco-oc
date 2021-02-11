b7bbe241940d5cd9a6eca755bb31a54db18de1a6
Thomas Claireau <thomas.claireau@gmail.com>
2021-02-11 07:22:34 +0100

    product list OK

--- src/components/products-list/index.js
+++ src/components/products-list/index.js
@@ -1,17 +1,54 @@
+import {renderPrice} from "../../functions.js"
+
 export default class ProductList extends HTMLElement {
     constructor() {
         super()
         this.url = "http://localhost:3000/api/cameras";
-        const products = this.getProducts()
-
-        console.log(products);
     }
 
-    getProducts() {
-        return fetch(this.url).then(res => res.json())
+    connectedCallback() {
+        this.getModel();
     }
 
-    renderProduct(data) {
-        return ``
+    getModel() {
+        return new Promise((res, rej) => {
+            fetch(this.url).then(res => res.json()).then(data => {
+                this.renderProducts(data)
+                res();
+            })
+                .catch((error) => rej(error));
+        })
+    }
+
+    renderProducts(data) {
+        this.innerHTML = `
+        <div class="products-list container">
+            ${data.map((product, index) => this.renderProduct(index, product)).join('')}
+        </div>
+        `
+    }
+
+    renderProduct(index, data) {
+        return `
+        <div class="product ${index % 2 ? 'right' : 'left'}">
+            <div class="image">
+                <img src="${data.imageUrl}" alt="">
+            </div>
+            <div class="infos">
+                <div class="title">${data.name}</div>
+                <div class="sizes">
+                    ${data.lenses.forEach(lense => `<span>${lense}</span>`)}
+                </div>
+                <div class="description">${data.description}</div>
+                <div class="price">${renderPrice(data.price)}</div>
+                <div class="actions">
+                    <a href="#" class="button">Voir ce produit</a>
+                    <button class="button add-to-cart" data-id="${data.id}">
+                        <svg-file file="./assets/img/shopping-cart.svg"></svg-file>
+                    </button>
+                </div>
+            </div>
+        </div>
+        `
     }
 }
---
+++ src/functions.js
@@ -0,0 +1,3 @@
+export function renderPrice(price) {
+    return `${price / 100} €`;
+}
