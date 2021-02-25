3388a784a1971b2756d9bcef1e421b596859f27d
Thomas Claireau <thomas.claireau@gmail.com>
2021-02-25 13:58:42 +0100

    wip product-cart (raf : gestion js de la suppression et de l'update qty)

--- src/products-cart.js
+++ src/products-cart.js
@@ -11,12 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const cart = new Cart()
     const ids = cart.renderCart()
 
-    Object.keys(ids).forEach((id) => {
+    Object.keys(ids).forEach((id, index) => {
         fetch(API_URL + "/" + id).then(res => res.json()).then(data => {
             productsCart.innerHTML += renderProduct(data, ids[id]);
         })
     })
+
+    // attendre l'insertion des produits dans le DOM
+    document.onreadystatechange = function () {
+        if (document.readyState === "complete") {
+            
+        }
+    }
 })
+
 
 function renderProduct(product, qty) {
     const unitPrice = renderPrice(product.price)
@@ -28,10 +36,17 @@ function renderProduct(product, qty) {
         <div class="infos">
             <div class="top">
                 <h2>${product.name}</h2>
-                <div class="close">cross</div>
+                <div class="close">
+                    <svg-file file="../assets/img/cancel.svg"></svg-file>
+                </div>
             </div>
             <div class="bottom">
-                <input type="number" value="${qty}">
+                <div class="qty">
+                    <span class="minus"></span>
+                    <span class="value">${qty}</span>
+                    <span class="plus"></span>
+                    <input type="hidden" name="qty" value="${qty}">
+                </div>
                 <div class="price">
                     <div>Prix unitaire : <strong>${unitPrice}</strong></div>
                     <div>Prix total : <strong>${price}</strong></div>
